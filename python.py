993a61d47332eb5ecbfe33a3fd97140cef9de3f3
Maksim Osminin <osmininmaks52@gmail.com>
2023-04-20 13:54:24 +0300

    Update python.py
    Использование остатка от деления для обнудения индекса при переполнении

--- python.py
+++ python.py
@@ -4,3 +4,10 @@ background_bench.append((mog_time, knn_time))
 mog_times, knn_times = zip(*background_bench) # Извлечение с помощью zip
 avg_mog = np.mean(mog_times)
 avg_knn = np.mean(knn_times)
+
+### Использование остатка от деления для обнудения индекса при переполнении
+max_values = 5
+index = 0
+for i in range(10):
+    index = (index + 1) % max_values
+    print(index)
