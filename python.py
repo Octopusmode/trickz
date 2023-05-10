e52f708e217757e821cc64a2d1ec81193650e5f1
Maksim Osminin <osmininmaks52@gmail.com>
2023-05-10 13:45:28 +0300

    Update python.py

--- python.py
+++ python.py
@@ -1,9 +1,13 @@
+### Этот код не предназначен для запуска
+
+
 # Извлечение каждого элемента в отдельный список
 background_bench.append((mog_time, knn_time))
 # mog_times, knn_times = [x[0] for x in background_bench], [x[1] for x in background_bench] # Извлечение с помощью генератора для каждого элемента
 mog_times, knn_times = zip(*background_bench) # Извлечение с помощью zip
 avg_mog = np.mean(mog_times)
 avg_knn = np.mean(knn_times)
+
 
 ### Использование остатка от деления для обнудения индекса при переполнении
 ## Иными словами циклическая инкрементация
@@ -13,7 +17,29 @@ for i in range(10):
     index = (index + 1) % max_values
     print(index)
     
+    
 ### Возврат нуля если значение отрицательное
 bar = max(0, foo)
 # То же для верхнего предела:
 bar = min(foo, max_value)
+
+### Слайс массива по столбцам
+rois[0]
+Out[7]: 
+array([[0.05364583, 0.27592593],
+       [0.225     , 0.325     ],
+       [0.1859375 , 0.5009259 ],
+       [0.02552083, 0.33240741]], dtype=float32)
+roi = rois[0]
+roi[:,0]
+Out[9]: array([0.05364583, 0.225     , 0.1859375 , 0.02552083], dtype=float32)
+roi[:,1]
+Out[10]: array([0.27592593, 0.325     , 0.5009259 , 0.33240741], dtype=float32)
+  
+## и последующее объединение
+# create two arrays
+x = np.array([1, 2, 3])
+y = np.array([4, 5, 6])
+
+# stack arrays by columns
+z = np.column_stack((x, y))
