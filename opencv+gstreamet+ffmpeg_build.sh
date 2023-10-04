cd46ec87bc770c49918b930e889e20ddbad1605f
Maksim Osminin <osmininmaks52@gmail.com>
2023-10-04 13:32:57 +0300

    Update opencv+gstreamet+ffmpeg_build.sh

--- opencv+gstreamet+ffmpeg_build.sh
+++ opencv+gstreamet+ffmpeg_build.sh
@@ -11,16 +11,16 @@ sudo apt -y install git make nasm pkg-config libx264-dev libxext-dev
 sudo wget -O ffmpeg-6.0.tar.bz2 "https://www.ffmpeg.org/releases/ffmpeg-6.0.tar.bz2"
 sudo tar -xvf ffmpeg-6.0.tar.bz2
 sudo rm -Rf ffmpeg-6.0.tar.bz2 
-cd /home/nomce/libs/ffmpeg-6.0
+cd ./ffmpeg-6.0
 ./configure --enable-nonfree --enable-gpl --enable-libx264 --enable-zlib
 make -j${nproc}
 sudo make install
 sudo ldconfig -v
 
 ## Compile Opencv with GStreamer and FFmpeg
-sudo apt-get install build-essential
-sudo apt-get install cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
-sudo apt-get install python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev
+sudo apt install build-essential
+sudo apt install cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
+sudo apt install python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev
 git clone https://github.com/opencv/opencv.git
 cd ~/opencv
 mkdir build
