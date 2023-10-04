fae66e725f2576b56e0981a1cea3fab714a81c3f
Maksim Osminin <osmininmaks52@gmail.com>
2023-10-04 14:32:28 +0300

    Update opencv+gstreamet+ffmpeg_build.sh

--- opencv+gstreamet+ffmpeg_build.sh
+++ opencv+gstreamet+ffmpeg_build.sh
@@ -32,7 +32,20 @@ rm -rf jasper*
 
 sudo apt install python3-dev python3-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libdc1394-dev
 git clone https://github.com/opencv/opencv.git
-cd ~/opencv
-mkdir build
-cd build
-cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D WITH_GSTREAMER=ON -D WITH_FFMPEG=ON ..
+mkdir /opencv_build && cd /opencv_build
+cmake -D CMAKE_BUILD_TYPE=RELEASE \
+    -D INSTALL_PYTHON_EXAMPLES=OFF \
+    -D INSTALL_C_EXAMPLES=OFF \
+    -D PYTHON_EXECUTABLE=$(which python3) \
+    -D PYTHON_DEFAULT_EXECUTABLE=$(which python3) \
+    -D BUILD_opencv_python2=OFF \
+    -D CMAKE_INSTALL_PREFIX=$(python3 -c "import sys; print(sys.prefix)") \
+    -D PYTHON3_EXECUTABLE=$(which python) \
+    -D PYTHON3_INCLUDE_DIR=$(python3 -c "from sysconfig import get_paths as gp; print(gp()[\"include\"])") \
+    -D PYTHON3_PACKAGES_PATH=$(python3 -c "import sysconfig; print(sysconfig.get_path('purelib'))") \
+    -D WITH_GSTREAMER=ON \
+    -D WITH_FFMPEG=ON \
+    -D WITH_TBB=OFF \
+    -D WITH_CUDA=OFF \
+    -D BUILD_EXAMPLES=OFF \
+    ..
