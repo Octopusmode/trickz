c7300b6aa0c33ffaf3bfb3d05109d6b5740f9bdb
Maksim Osminin <osmininmaks52@gmail.com>
2024-06-17 18:30:38 +0300

    Update python_examples.py

--- python_examples.py
+++ python_examples.py
@@ -77,3 +77,12 @@ def time_tracker(func):
 
     return wrapper
 
+### Функция перевода в произвольную систему счисления
+def convert_to(number, base, upper=False):
+    digits = '0123456789abcdefghijklmnopqrstuvwxyz'
+    if base > len(digits): return None
+    result = ''
+    while number > 0:
+        result = digits[number % base] + result
+        number //= base
+    return result.upper() if upper else result
